0791074afb43d98645a501ebf3bfd3ee73421b59
driftluo <driftluo@foxmail.com>
2023-04-21 11:20:33 +0800

    feat: support custom signer

--- secio/src/lib.rs
+++ secio/src/lib.rs
@@ -63,7 +63,9 @@ impl SecioKeyPair {
         match self.inner {
             KeyPairInner::Secp256k1 { ref private } => {
                 let pubkey = crate::secp256k1_compat::from_secret_key(private);
-                PublicKey::Secp256k1(crate::secp256k1_compat::serialize_pubkey(&pubkey))
+                PublicKey {
+                    key: crate::secp256k1_compat::serialize_pubkey(&pubkey),
+                }
             }
         }
     }
@@ -74,11 +76,17 @@ impl SecioKeyPair {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 enum KeyPairInner {
     Secp256k1 {
         private: crate::secp256k1_compat::SecretKey,
     },
+}
+
+impl std::fmt::Debug for KeyPairInner {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("KeyPair").finish()
+    }
 }
 
 /// Possible digest algorithms.
@@ -98,5 +106,148 @@ impl Digest {
             Digest::Sha256 => 256 / 8,
             Digest::Sha512 => 512 / 8,
         }
+    }
+}
+
+/// Signer on ecdh procedure
+#[cfg_attr(all(target_arch = "wasm32", feature = "async-trait"), async_trait::async_trait(?Send))]
+#[cfg_attr(
+    all(not(target_arch = "wasm32"), feature = "async-trait"),
+    async_trait::async_trait
+)]
+pub trait Signer: std::fmt::Debug + Send + Sync + 'static {
+    /// Error
+    type Error: Into<crate::error::SecioError>;
+    /// Public key
+    type Pubkey: Pubkey;
+
+    /// Constructs a signature for `msg` using the secret key `sk`
+    #[cfg(feature = "async-trait")]
+    async fn sign_ecdsa_async<T: AsRef<[u8]> + Send>(
+        &self,
+        message: T,
+    ) -> Result<Vec<u8>, Self::Error> {
+        self.sign_ecdsa(message)
+    }
+
+    /// Constructs a signature for `msg` using the secret key `sk`
+    fn sign_ecdsa<T: AsRef<[u8]> + Send>(&self, message: T) -> Result<Vec<u8>, Self::Error>;
+
+    /// Creates a new public key from a [`Signer`].
+    fn pubkey(&self) -> Self::Pubkey;
+
+    /// Recover public key from slice
+    fn pubkey_from_slice<T: AsRef<[u8]>>(key: T) -> Result<Self::Pubkey, Self::Error>;
+}
+
+/// Public key for Signer
+pub trait Pubkey: std::fmt::Debug + Send + Sync + 'static {
+    /// Checks that `sig` is a valid ECDSA signature for `msg` using the public
+    /// key `pubkey`.
+    fn verify_ecdsa<T: AsRef<[u8]>, F: AsRef<[u8]>>(&self, message: T, signature: F) -> bool;
+
+    /// serialized key into a bytes
+    fn serialize(&self) -> Vec<u8>;
+}
+
+impl Signer for SecioKeyPair {
+    type Error = error::SecioError;
+    type Pubkey = secp256k1_compat::PublicKey;
+
+    fn sign_ecdsa<T: AsRef<[u8]> + Send>(&self, message: T) -> Result<Vec<u8>, Self::Error> {
+        let msg = match crate::secp256k1_compat::message_from_slice(message.as_ref()) {
+            Ok(m) => m,
+            Err(_) => {
+                log::debug!("message has wrong format");
+                return Err(error::SecioError::InvalidMessage);
+            }
+        };
+        let signature = match self.inner {
+            KeyPairInner::Secp256k1 { ref private } => crate::secp256k1_compat::sign(&msg, private),
+        };
+
+        Ok(crate::secp256k1_compat::signature_to_vec(signature))
+    }
+
+    fn pubkey(&self) -> Self::Pubkey {
+        match self.inner {
+            KeyPairInner::Secp256k1 { ref private } => {
+                crate::secp256k1_compat::from_secret_key(private)
+            }
+        }
+    }
+
+    fn pubkey_from_slice<T: AsRef<[u8]>>(key: T) -> Result<Self::Pubkey, Self::Error> {
+        crate::secp256k1_compat::pubkey_from_slice(key.as_ref())
+            .map_err(|_| crate::error::SecioError::SecretGenerationFailed)
+    }
+}
+
+impl Pubkey for secp256k1_compat::PublicKey {
+    fn verify_ecdsa<T: AsRef<[u8]>, F: AsRef<[u8]>>(&self, message: T, signature: F) -> bool {
+        let signature = crate::secp256k1_compat::signature_from_der(signature.as_ref());
+        let msg = crate::secp256k1_compat::message_from_slice(message.as_ref());
+
+        if let (Ok(signature), Ok(message)) = (signature, msg) {
+            if !crate::secp256k1_compat::verify(&message, &signature, self) {
+                log::debug!("failed to verify the remote's signature");
+                return false;
+            }
+        } else {
+            log::debug!("remote's secp256k1 signature has wrong format");
+            return false;
+        }
+        true
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        crate::secp256k1_compat::serialize_pubkey(self)
+    }
+}
+
+impl<T> Signer for std::sync::Arc<T>
+where
+    T: Signer,
+{
+    type Error = <T as Signer>::Error;
+    type Pubkey = <T as Signer>::Pubkey;
+
+    fn sign_ecdsa<F: AsRef<[u8]> + Send>(&self, message: F) -> Result<Vec<u8>, Self::Error> {
+        self.as_ref().sign_ecdsa(message)
+    }
+
+    fn pubkey(&self) -> Self::Pubkey {
+        self.as_ref().pubkey()
+    }
+
+    fn pubkey_from_slice<F: AsRef<[u8]>>(key: F) -> Result<Self::Pubkey, Self::Error> {
+        <T as Signer>::pubkey_from_slice(key)
+    }
+}
+
+impl Signer for () {
+    type Error = error::SecioError;
+    type Pubkey = ();
+
+    fn sign_ecdsa<T: AsRef<[u8]> + Send>(&self, _message: T) -> Result<Vec<u8>, Self::Error> {
+        Err(error::SecioError::NotSupportSigner)
+    }
+
+    fn pubkey(&self) -> Self::Pubkey {
+        ()
+    }
+
+    fn pubkey_from_slice<T: AsRef<[u8]>>(_key: T) -> Result<Self::Pubkey, Self::Error> {
+        Ok(())
+    }
+}
+
+impl Pubkey for () {
+    fn verify_ecdsa<T: AsRef<[u8]>, F: AsRef<[u8]>>(&self, _message: T, _signature: F) -> bool {
+        false
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        Vec::new()
     }
 }
