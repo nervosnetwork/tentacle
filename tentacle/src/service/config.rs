8b145c14c283ed7b694ca287bd5896a634746c9c
Eval EXEC <execvy@gmail.com>
2024-12-23 16:02:36 +0800

    Add `TransformerContext` to `TcpSocketTransformer`

--- tentacle/src/service/config.rs
+++ tentacle/src/service/config.rs
@@ -13,7 +13,7 @@ use std::os::{
     fd::AsFd,
     unix::io::{AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, RawFd},
 };
-use std::{sync::Arc, time::Duration};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
 #[cfg(feature = "tls")]
 use tokio_rustls::rustls::{ClientConfig, ServerConfig};
 
@@ -93,8 +93,46 @@ pub struct ProxyConfig {
     pub proxy_url: String,
 }
 
-pub(crate) type TcpSocketTransformer =
-    Arc<dyn Fn(TcpSocket) -> Result<TcpSocket, std::io::Error> + Send + Sync + 'static>;
+/// This enum's purpose is to let socket_transformer know how the socket is created
+pub enum SocketState {
+    /// listen
+    Listen,
+    /// dial
+    Dial,
+}
+
+/// in socket_transformer
+pub struct TransformerContext {
+    /// dial or listen
+    pub state: SocketState,
+    /// if dial, remote address; if listen, local address
+    pub address: SocketAddr,
+}
+
+impl TransformerContext {
+    /// crate a listen context
+    pub fn new_listen(address: SocketAddr) -> Self {
+        TransformerContext {
+            state: SocketState::Listen,
+            address,
+        }
+    }
+
+    /// create a dial context
+    pub fn new_dial(address: SocketAddr) -> Self {
+        TransformerContext {
+            state: SocketState::Dial,
+            address,
+        }
+    }
+}
+
+pub(crate) type TcpSocketTransformer = Arc<
+    dyn Fn(TcpSocket, TransformerContext) -> Result<TcpSocket, std::io::Error>
+        + Send
+        + Sync
+        + 'static,
+>;
 
 #[derive(Clone)]
 pub(crate) struct TcpSocketConfig {
@@ -105,7 +143,7 @@ pub(crate) struct TcpSocketConfig {
 impl Default for TcpSocketConfig {
     fn default() -> Self {
         Self {
-            socket_transformer: Arc::new(Ok),
+            socket_transformer: Arc::new(|tcp_socket, _| Ok(tcp_socket)),
             proxy_config: None,
         }
     }
