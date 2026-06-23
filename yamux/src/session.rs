c31418834c9abd63acc45cde1059a7aa3d78a1f0
officeyutong <yt.xyxx@gmail.com>
2026-06-23 17:03:21 +0800

    update

--- yamux/src/session.rs
+++ yamux/src/session.rs
@@ -73,6 +73,8 @@ pub struct Session<T> {
     pending_streams: VecDeque<StreamHandle>,
     // The buffer which will send to underlying network
     write_pending_frames: VecDeque<Frame>,
+    // Last time the framed sink accepted an outbound frame.
+    last_send_success: Instant,
     // The buffer which will distribute to sub streams
     read_pending_frames: VecDeque<Frame>,
 
@@ -179,6 +181,10 @@ where
             streams: HashMap::default(),
             pending_streams: VecDeque::default(),
             write_pending_frames: VecDeque::default(),
+            #[cfg(not(all(target_family = "wasm", not(target_os = "unknown"))))]
+            last_send_success: Instant::now(),
+            #[cfg(all(target_family = "wasm", not(target_os = "unknown")))]
+            last_send_success: Instant::from_u64(0),
             read_pending_frames: VecDeque::default(),
             event_sender,
             event_receiver,
@@ -246,6 +252,11 @@ where
         };
         let frame = Frame::new_ping(Flags::from(flag), ping_id);
         self.send_frame(cx, frame).map(|_| ping_id)
+    }
+
+    fn write_stalled(&self) -> bool {
+        self.now().saturating_duration_since(self.last_send_success)
+            > self.config.connection_write_timeout
     }
 
     /// GoAway can be used to prevent accepting further
@@ -357,10 +368,15 @@ where
             match sink.as_mut().poll_ready(cx)? {
                 Poll::Ready(()) => {
                     sink.as_mut().start_send(frame)?;
+                    self.last_send_success = self.now();
                 }
                 Poll::Pending => {
                     debug!("[{:?}] framed_stream NotReady, frame: {:?}", self.ty, frame);
                     self.write_pending_frames.push_front(frame);
+
+                    if self.write_stalled() {
+                        return Err(io::ErrorKind::TimedOut.into());
+                    }
 
                     if self.poll_complete(cx)? {
                         return Ok(true);
@@ -379,7 +395,13 @@ where
     /// Sink `poll_complete` NotReady -> there is more work left to do, may wake up next poll
     fn poll_complete(&mut self, cx: &mut Context) -> Result<bool, io::Error> {
         match Pin::new(&mut self.framed_stream).poll_flush(cx) {
-            Poll::Pending => Ok(true),
+            Poll::Pending => {
+                if self.write_stalled() {
+                    Err(io::ErrorKind::TimedOut.into())
+                } else {
+                    Ok(true)
+                }
+            }
             Poll::Ready(res) => res.map(|_| false),
         }
     }
@@ -1040,6 +1062,21 @@ mod test {
     }
 
     // after TIMEOUT time, will finished
+    #[test]
+    fn test_write_stalled_uses_connection_write_timeout() {
+        let (_remote, local) = MockSocket::new();
+        let config = Config {
+            enable_keepalive: false,
+            connection_write_timeout: Duration::from_millis(1),
+            ..Default::default()
+        };
+        let mut session = Session::new_server(local, config);
+        assert!(!session.write_stalled());
+
+        session.last_send_success = session.now() - Duration::from_secs(1);
+        assert!(session.write_stalled());
+    }
+
     #[test]
     fn test_keepalive_should_work_on_no_communication_scenario() {
         let rt = rt();
